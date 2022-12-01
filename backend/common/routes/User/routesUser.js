597dfb2800395c784159cbfbeb550d17dc61e026
Arthur-Ponty <arthur.ponty76@gmail.com>
2022-12-01 23:27:53 +0100

    Finish backend

--- backend/common/routes/User/routesUser.js
+++ backend/common/routes/User/routesUser.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userModel = require("../../schema/User/schemaUser");
 const app = express();
 const router = express.Router();
+
 router.post("/", async (request, response) => {
     const user = new userModel(request.body);
 
@@ -14,4 +15,27 @@ router.post("/", async (request, response) => {
         response.status(500).send(error);
     }
 });
+
+router.get("/", async (request, response) => {
+    var users = await userModel.find({});
+    
+    if(!users) {
+        response.status(404).json({message : "No users found"});
+        return;
+    }
+
+    response.status(200).send(users);
+})
+
+router.get("/:username", async (request, response) => {
+    var user = await userModel.find({ username : request.params.username });
+
+    if(!user) {
+        response.status(404).json({message : "User not found"});
+        return;
+    }
+
+    response.status(200).json(user[0]);
+})
+
 module.exports = router;
